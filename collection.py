bc70fc0944a800d7989bd33ccf20cc8460c06647
Kochi Exidos Menang <kochiexidos700@gmail.com>
2026-02-13 04:18:38 -1000

    Add files via upload

---
+++ collection.py
@@ -0,0 +1,25 @@
+myfruitlist = ["apple", "banana", "cherry"]
+print(myfruitlist)
+print(type(myfruitlist))
+
+print(myfruitlist[0])
+print(myfruitlist[1])
+print(myfruitlist[2])
+
+myfruitlist[2] = "Orange"
+print(myfruitlist)
+
+myfinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myfinalAnswerTuple)
+print(type(myfinalAnswerTuple))
+
+print(myfinalAnswerTuple[0])
+print(myfinalAnswerTuple[1])
+print(myfinalAnswerTuple[2])
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
